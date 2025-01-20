f44b856a72921d60e4425914bedf693ff019ed25
Abderahmanvt7 <Abderahmanvt7@gmail.com>
2025-01-20 21:42:04 +0100

    first commit

---
+++ MobileSample/Models/MainMenuItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileSample.Models
+{
+    public enum MenuItemType
+    {
+        About,
+        DepartmentList,
+        EmployeeList,
+        Setup
+    }
+
+    public class MainMenuItem
+    {
+        public MenuItemType Id { get; set; }
+        public string Title { get; set; }
+
+        public static List<MainMenuItem> GetMenus()
+        {
+            List<MainMenuItem> menus = new List<MainMenuItem>();
+            menus.Add(new MainMenuItem() { Id = MenuItemType.About, Title = "Accueil" });
+            menus.Add(new MainMenuItem() { Id = MenuItemType.DepartmentList, Title = "Départements" });
+            menus.Add(new MainMenuItem() { Id = MenuItemType.EmployeeList, Title = "Employés" });
+            return menus;
+        }
+    }
+}
---
+++ MobileSample/ViewModels/DepartmentItemViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using MobileSample.Models;
+using MobileSample.Services;
+
+namespace MobileSample.ViewModels
+{
+    public class DepartmentItemViewModel : BaseViewModel
+    {
+        private readonly INavigation navigation;
+        private readonly DatabaseService databaseService;
+
+        public DepartmentItemViewModel(INavigation navigation, DatabaseService databaseService)
+        {
+            this.navigation = navigation;
+            this.databaseService = databaseService;
+        }
+
+        private bool isNewData;
+        public bool IsNewData
+        {
+            get { return isNewData; }
+            set { SetProperty(ref isNewData, value);  }
+        }
+
+        private Department data;
+        public Department Data
+        {
+            get { return data; }
+            set { SetProperty(ref data, value); }
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+            Data = (Department)initData;
+            if (Data == null)
+            {
+                Title = $"Ajouter un département";
+                isNewData = true;
+                Data = new Department();
+                Data.Id = Guid.NewGuid().ToString();
+            }
+            else
+            {
+                Title = $"Modifier un département";
+            }
+        }
+
+        public ICommand SaveCommand => new Command(async () =>
+        {
+            using (var databaseContext = databaseService.GetDatabaseContext())
+            {
+                if (IsNewData)
+                {
+                    databaseContext.Add(Data);
+                }
+                else
+                {
+                    databaseContext.Update(Data);
+                }
+                await databaseContext.SaveChangesAsync();   
+            }
+
+            await navigation.PopAsync();
+        });
+
+        public ICommand DeleteCommand => new Command(async () =>
+        {
+            if (IsNewData) return;
+
+            using (var databaseContext = databaseService.GetDatabaseContext())
+            {
+                Employee dependentEmployee = databaseService.GetEmployees(databaseContext).Where(w => w.DepartmentId == Data.Id).FirstOrDefault();
+                if (dependentEmployee != null)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Attention", $"Ce département ne peut pas être supprimé car il contient des employés", "OK");
+                    return;
+                }
+
+                databaseContext.Remove(Data);
+
+                await databaseContext.SaveChangesAsync();
+            }
+
+            await navigation.PopAsync();
+        });
+    }
+}
